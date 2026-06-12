5b88b8a63eb57ee08b25fd1016f6a763b4b6e9b5
ethancheph-jpg <ethan.che.ph@gmail.com>
2026-06-12 19:28:59 +0800

    download_app.py

--- download_app.py
+++ download_app.py
@@ -4,7 +4,7 @@ st.title('dowload the green and red app')
 
 st.write('press the button and dowlaod from your phone')
 
-app_url = f''
+app_url = f'https://drive.google.com/uc?export=download&id=1MhX1xwzqeXPgjPmkrapBV7NfNTYOLZzS'
 
 st.link_button(
     label='download from me',
